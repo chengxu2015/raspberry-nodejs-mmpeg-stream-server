49965eed6f1be0a0f5597b9795c8ae501422ab81
Ji Xuan <404494394@qq.com>
2017-10-21 21:03:19 +0800

    [jixuan] 100 init

---
+++ start.sh
@@ -0,0 +1,1 @@
+node websocket-relay.js supersecret 8081 8082 & http-server & ffmpeg -f avfoundation -video_size 1280x720 -framerate 30 -i "0:0" -f mpegts -codec:v mpeg1video -s 1280x720 -b:v 1000k -bf 0 http://localhost:8081/supersecret
